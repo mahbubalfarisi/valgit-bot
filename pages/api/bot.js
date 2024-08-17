4e793d543c76fe7d60054e5a28009dd10b851217
mahbub.alfarisi <mahbub.alfarisi@primavista-solusi.com>
2024-08-17 21:49:30 +0700

    Major Changes

---
+++ pages/api/bot.js
@@ -0,0 +1,26 @@
+// pages/api/bot.js
+import { Client } from 'discord.js';
+
+const client = new Client({
+  intents: [
+    'GUILDS',
+    'GUILD_MESSAGES',
+    'MESSAGE_CONTENT',
+  ],
+});
+
+client.once('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on('messageCreate', message => {
+  if (message.content === '!ping') {
+    message.channel.send('Pong!');
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
+
+export default async function handler(req, res) {
+  res.status(200).json({ message: 'Bot is running!' });
+}
